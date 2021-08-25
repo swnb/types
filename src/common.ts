45d9a6cdd8e7de615e608eec3768086a10791a7c
swnb <swnblovego@gmail.com>
2021-08-25 09:19:09 +0800

    feat(arguments): add new type extrack arguments

--- src/common.ts
+++ src/common.ts
@@ -3,3 +3,7 @@ export type Fn<Args extends any[] = [], Result = void> = (...args: Args) => Resu
 export type TypeArray<T, N extends number, R extends T[] = []> = R['length'] extends N
   ? R
   : TypeArray<T, N, [...R, T]>
+
+export type Arguments<T extends (...args: any[]) => any> = T extends (...args: infer Args) => any
+  ? Args
+  : never
